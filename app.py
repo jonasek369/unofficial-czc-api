57367b7cfd343ec80c77a57b172b956e2e3f4969
jonasek369 <69902785+jonasek369@users.noreply.github.com>
2022-03-27 21:10:01 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-import json
 import os
 
-import redis
 from flask import Flask, render_template, request
 
-from Logger import *
 from czcParser import Parser
 from errors import *
 
@@ -12,17 +9,9 @@ parser = Parser()
 
 app = Flask(__name__)
 
-DEFAULT_REDIS_EXPIRE = 600
-rds = redis.Redis()
 
 os.system("cls")
 
-try:
-    rds.keys()
-    LOG(SUCCESS, "Connected to redis database")
-except redis.exceptions.ConnectionError or any:
-    LOG(ERROR, "Connection to redis database failed")
-    exit(1)
 
 
 @app.route('/')
@@ -35,15 +24,9 @@ def product_info():
     try:
         data = request.get_json()
         identifier = data.get('identifier')
-        redis_fetch = rds.get(f"product-info:{identifier}")
-        if redis_fetch is None:
-            if identifier is None:
-                return throw_missing_parameters(["identifier"])
-            data = parser.get_product_info(f"https://www.czc.cz/a/{identifier}/produkt")
-            rds.setex(f"product-info:{identifier}", DEFAULT_REDIS_EXPIRE, json.dumps(data))
-            return data
-        else:
-            return json.loads(redis_fetch)
+        if identifier is None:
+            return throw_missing_parameters(["identifier"])
+        return parser.get_product_info(f"https://www.czc.cz/a/{identifier}/produkt")
     except KeyError:
         return throw_json_error()
 
@@ -53,14 +36,8 @@ def list_info():
     try:
         data = request.get_json()
         identifier = data.get('identifier')
-        redis_fetch = rds.get(f"list-info:{identifier}")
-        if redis_fetch is None:
-            if identifier is None:
-                return throw_missing_parameters(["identifier"])
-            data = parser.get_seznam_info(f"https://www.czc.cz/{identifier}/seznam")
-            rds.setex(f"list-info:{identifier}", DEFAULT_REDIS_EXPIRE, json.dumps(data))
-            return data
-        else:
-            return json.loads(redis_fetch)
+        if identifier is None:
+            return throw_missing_parameters(["identifier"])
+        return parser.get_seznam_info(f"https://www.czc.cz/{identifier}/seznam")
     except KeyError:
         return throw_json_error()

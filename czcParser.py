83f9a2ec6f3c49347c72c1673c078a20454d1705
jonasek369 <69902785+jonasek369@users.noreply.github.com>
2022-10-14 20:34:54 +0200

    Commited the wrong file.

--- czcParser.py
+++ czcParser.py
@@ -3,87 +3,78 @@ import re
 import time
 
 import requests
-
-MAX_FL_ITERS = 8
+import validators
 
 
 class Parser:
     def __init__(self):
+        self.session = requests.session()
         self.json_regex = "\{.*?\}"
+        self.PRODUCT_MEMBERS_TO_REMOVE = [
+            "worker",
+            "http_session_id",
+            "userLoggedin",
+        ]
 
-    def _find_last(self, obj, text):
-        nums = [0]
-        for i in range(MAX_FL_ITERS):
-            if text.count(obj) > 2:
-                fnd = text.find(obj, sorted(nums, reverse=True)[0] - 1)
-            else:
-                fnd = text.find(obj, sorted(nums, reverse=True)[0])
-            if fnd != -1:
-                nums.append(fnd)
-            if fnd == sorted(nums, reverse=True)[0]:
+    @staticmethod
+    def url_from_id(pid):
+        return f"https://www.czc.cz/a/{pid}/produkt"
+
+    @staticmethod
+    def data_find(data, start, end, cut_star=True, addstart=0, addend=0):
+        findstart = 0 if start is None or start is False else data.find(start)
+        findend = len(data) if end is None or end is False else data.find(end)
+
+        if cut_star and start is not None:
+            return data[findstart + len(start) + addstart:findend + addend]
+        else:
+            return data[findstart + addstart:findend + addend]
+
+    def parse_product(self, product_id):
+        if validators.url(product_id) and product_id.startswith("https://www.czc.cz/"):
+            url = product_id
+        else:
+            url = self.url_from_id(product_id)
+        req = self.session.get(url).text
+        datalayer_start = self.data_find(req, "dataLayer = ", None, True)  # req[req.find("dataLayer"):]
+        datalayer_lists = self.data_find(datalayer_start, None, "</script>", addend=-1).split(";")
+        selected = ""
+        for i in datalayer_lists:
+            if i[0] == "e":
+                selected = i
                 break
-        return sorted(nums, reverse=True)[0] + 1
+        if not selected:
+            return "ERROR: NO expone DataLayer"
 
-    def get_product_info(self, url):
-        try:
-            req = requests.get(url).text
-            datalayer_list = req[req.find("dataLayer") + 12:req.find("</script>")].split(";")
-            datalayer_jsons = []
-            for dl_list in datalayer_list:
-                if dl_list == "" and "stock_level" not in dl_list:
-                    continue
-                new_dl = dl_list[dl_list.find("{"):self._find_last("}", dl_list)]
-                datalayer_jsons.append(json.loads(new_dl))
-            nj = datalayer_jsons[-1]
-            return {
-                "status": "SUCCESS",
-                "name": nj["pageview"]["data"].get("title"),
-                "price_woVAT": nj["pageview"]["data"].get("czc_club_price_woVAT"),
-                "price": nj["pageview"]["data"].get("discount_price_VAT"),
-                "in_stock": True if nj["pageview"]["data"]["stock_level"] != 0 else False,
-                "discount_value": nj["pageview"]["data"].get("discount_value"),
-                "discount_percentage": nj["pageview"]["data"].get("discount_percentage")
-            }
-        except:
-            return {
-                "status": "ERROR",
-                "message": "there was an error during data gathering phase please ensure that the identifier is right"
-            }
+        to_json = json.loads(selected[20:len(selected) - 1])
 
-    def get_seznam_info(self, url):
-        try:
-            req = requests.get(url).text
-            jsons = re.findall(self.json_regex, req.strip())
-            name_val = req.find('<span itemprop="name">') + 22
-            name = req[name_val:req.find("</span>", name_val)]
-            products = []
-            total_price = 0
-            for i in jsons:
-                try:
-                    prod = json.loads(i)
-                    if prod.get("price") is None or prod.get("name") is None:
-                        continue
-                    products.append(prod)
-                except ValueError as e:
-                    continue
-            products = sorted(products, key=lambda d: d["position"])
-            for i in products:
-                total_price += i["price"]
-            return {
-                "name": name,
-                "total_price": total_price,
-                "products": [i for i in products if i.pop("id") is not None]
-            }
-        except:
-            return {
-                "status": "ERROR",
-                "message": "there was an error during data gathering phase please ensure that the identifier is right"
-            }
+        return {
+            key: val for key, val in to_json["data"].items() if key not in self.PRODUCT_MEMBERS_TO_REMOVE
+        }
+
+    def parse_seznam(self, seznam_id):
+        if validators.url(seznam_id) and seznam_id.startswith("https://www.czc.cz/"):
+            url = seznam_id
+        else:
+            url = f"https://www.czc.cz/{seznam_id}/seznam"
+        req = self.session.get(url).text
+        name_val = req.find('<span itemprop="name">') + 22
+        name = req[name_val:req.find("</span>", name_val)]
+        jsons = re.findall(self.json_regex, req.strip())
+        product_jsons = []
+        for _json in jsons:
+            if '"price"' in _json:
+                product_jsons.append(json.loads(_json))
+        return {
+            "name": name,
+            "total_price": sum([i["price"] for i in product_jsons]),
+            "prducts": product_jsons
+        }
 
 
-if __name__ == '__main__':
-    p = Parser()
-    start = time.perf_counter()
-    # print(p.get_seznam_info("https://www.czc.cz/9todcb1sbshncb4hg8qnfafbtg/seznam"))
-    # print(p.get_product_info("https://www.czc.cz/sony-ps5-bezdratovy-ovladac-dualsense-cosmic-red/318356/produkt"))
-    print(time.perf_counter() - start)
+
+parser = Parser()
+start = time.perf_counter()
+print(parser.parse_seznam(("bol5a64d9shh2biahcn4t8s0oa")))
+end = time.perf_counter()
+print((end - start) * 1000, "ms")
